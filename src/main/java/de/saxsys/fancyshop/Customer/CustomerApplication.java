48b2e3b3b6d9435a982b09d1e0e4505e402f1940
Dudziec <damian.dudziec@saxsys.de>
2018-11-07 14:12:58 +0100

    Added method

--- src/main/java/de/saxsys/fancyshop/Customer/CustomerApplication.java
+++ src/main/java/de/saxsys/fancyshop/Customer/CustomerApplication.java
@@ -2,11 +2,18 @@ package de.saxsys.fancyshop.Customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerApplication {
+public class CustomerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CustomerApplication.class);
 	}
 }
